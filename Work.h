6ec70238a5a5377274496639152ae323a9874f43
jlangr <jeff@langrsoft.com>
2013-04-24 11:06:17 -0600

    init

---
+++ Work.h
@@ -0,0 +1,22 @@
+#ifndef Work_h
+#define Work_h
+
+#include <functional>
+
+class Work {
+public:
+   Work() : executeFunction_{[] () {}} {}
+
+   Work(std::function<void()> executeFunction) 
+      : executeFunction_(executeFunction) {}
+
+   void execute() {
+      executeFunction_();
+   }
+
+private:
+   std::function<void()> executeFunction_;
+};
+
+#endif
+
---
+++ WorkTest.cpp
@@ -0,0 +1,54 @@
+#include "CppUTest/TestHarness.h"
+
+#include "Work.h"
+
+#include <functional>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+TEST_GROUP(AWorkObject) {
+};
+
+TEST(AWorkObject, ExecutesFunctionStored) {
+   bool wasExecuted{false};
+   auto executeFunction = [&] () { wasExecuted = true; };
+   Work work(executeFunction);
+   
+   work.execute();
+
+   CHECK_TRUE(wasExecuted);
+}
+
+TEST(AWorkObject, CreatedWithDefaultConstructorDoesNothingWhenExecuted) {
+   Work work;
+   try {
+      
+      work.execute();
+
+   }
+   catch (...) {
+      FAIL("no-op should not throw");
+   }
+}
+
+TEST(AWorkObject, CanExecuteOnDataCapturedWithFunction) {
+   vector<string> data{"a", "b"};
+   string result;
+   auto callbackFunction = [&] (string s) {
+      result.append(s);
+   };
+
+   auto executeFunction = [&] () {
+      stringstream s;
+      s << data[0] << data[1];
+      callbackFunction(s.str());
+   };
+   Work work(executeFunction);
+
+   work.execute();
+
+   CHECK_EQUAL("ab", result);
+}
+
