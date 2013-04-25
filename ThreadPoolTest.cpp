7e0be443bf12cb8e98f1540309c218abd41a8f75
jlangr <jeff@langrsoft.com>
2013-04-24 20:38:38 -0600

    timeotu

--- ThreadPoolTest.cpp
+++ ThreadPoolTest.cpp
@@ -14,14 +14,17 @@ TEST_GROUP(AThreadPool) {
 
 TEST(AThreadPool, ProcessesASingleRequest) {
    ThreadPool pool;
-   condition_variable assertFunctionWasExecuted;
+   condition_variable wasExecuted;
    auto executeFunction = [&] () { 
-      assertFunctionWasExecuted.notify_one();
+      wasExecuted.notify_one();
    };
    Work work(executeFunction);
    
    pool.add(work);
 
    unique_lock<mutex> lock(m);
-   assertFunctionWasExecuted.wait(lock);
+   chrono::milliseconds timeout(100);
+   CHECK_FALSE(wasExecuted.wait_for(lock, timeout) 
+         == cv_status::timeout);
 }
+
