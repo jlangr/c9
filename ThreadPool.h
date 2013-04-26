faa1deeede3d92f999731984e449cf6242142e0f
jlangr <jeff@langrsoft.com>
2013-04-26 10:10:56 -0600

    fix

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,28 +1,21 @@
 #ifndef ThreadPool_h
 #define ThreadPool_h
 
-// START:thread
 #include <string>
 #include <deque>
 #include <thread>
 #include <memory>
-// START_HIGHLIGHT
 #include <atomic>
-// END_HIGHLIGHT
 
 #include "Work.h"
 
 class ThreadPool {
 public:
    virtual ~ThreadPool() {
-// START_HIGHLIGHT
       done_ = true;
-// END_HIGHLIGHT
       if (workThread_)
          workThread_->join();
    }
-   // ...
-// END:thread
    void start() {
       workThread_ = std::make_shared<std::thread>(&ThreadPool::worker, this);
    }
@@ -41,26 +34,23 @@ public:
       return work;
    }
 
-// START:thread
 private:
+// START:thread
    void worker() {
-// START_HIGHLIGHT
       while (!done_) {
-// END_HIGHLIGHT
-         while (!hasWork())
+// START_HIGHLIGHT
+         while (!done_ && !hasWork()) 
             ;
+         if (done_) break;
+// END_HIGHLIGHT
          pullWork().execute();
-// START_HIGHLIGHT
       }
-// END_HIGHLIGHT
    }
+// END:thread
 
-// START_HIGHLIGHT
    std::atomic<bool> done_{false};
-// END_HIGHLIGHT
    std::deque<Work> workQueue_;
    std::shared_ptr<std::thread> workThread_;
 };
-// END:thread
 
 #endif
