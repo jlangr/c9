8089a6befea34f24d5909ced2980346755c967ad
jlangr <jeff@langrsoft.com>
2013-04-29 22:21:36 -0600

    ab

--- GeoServerTest.cpp
+++ GeoServerTest.cpp
@@ -50,7 +50,7 @@ TEST(AGeoServer, UpdatesLocationOfUser) {
 }
 
 TEST(AGeoServer, AnswersUnknownLocationForUserNotTracked) {
-   CHECK_TRUE(server.locationOf("aAbUser").isUnknown());
+   CHECK_TRUE(server.locationOf("anAbUser").isUnknown());
 }
 
 TEST(AGeoServer, AnswersUnknownLocationForTrackedUserWithNoLocationUpdate) {
