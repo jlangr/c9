e1a95864341daa5462ae4bcbb169ada73ff73b3d
jlangr <jeff@langrsoft.com>
2013-05-23 21:50:47 -0600

    longs_equal

--- GeoServerTest.cpp
+++ GeoServerTest.cpp
@@ -145,6 +145,6 @@ IGNORE_TEST(AGeoServer_UsersInBox, HandlesLargeNumbersOfUsers) {
    TestTimer timer;
    auto users = server.usersInBox(aUser, Width, Height);
 
-   CHECK_EQUAL(lots, users.size());
+   LONGS_EQUAL(lots, users.size());
 }
 
