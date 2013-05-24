475ec9c45b2ed1ddf58914acfe8e2fa19a29fb62
jlangr <jeff@langrsoft.com>
2013-05-23 20:13:19 -0600

    override

--- GeoServerTest.cpp
+++ GeoServerTest.cpp
@@ -116,7 +116,7 @@ public:
 
    shared_ptr<ThreadPool> pool;
 
-   virtual void setup() {
+   virtual void setup() override {
       server.useThreadPool(pool);
 
       server.track(aUser);
@@ -147,7 +147,7 @@ TEST_GROUP_BASE(AGeoServer_UsersInBox, GeoServerUsersInBoxTests) {
       virtual void add(Work work) override { work.execute(); }
    };
 
-   void setup() {
+   void setup() override {
       pool = make_shared<SingleThreadedPool>();
       GeoServerUsersInBoxTests::setup();
    }
@@ -215,12 +215,12 @@ TEST_GROUP_BASE(AGeoServer_ScaleTests, GeoServerUsersInBoxTests) {
    GeoServerCountingListener countingListener;
    shared_ptr<thread> t;
 
-   void setup() {
+   void setup() override {
       pool = make_shared<ThreadPool>();
       GeoServerUsersInBoxTests::setup();
    }
 
-   void teardown() {
+   void teardown() override {
       t->join();
    }
 };
